d377df149277de650b74f705f61787e059607ea2
Ivan Galarza <ivang@bluetrailsoft.com>
2023-10-17 12:40:25 -0400

    ota endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable, of } from 'rxjs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
 
   @Get()
-  getHello(): string {
+  hello(): String {
     return this.appService.getHello();
+  }
+
+  @Get('apk/version/:filename')
+  downloadFile(@Param('filename') filename, @Res() res): Observable<String> {
+    console.log("request")
+    return of(res.sendFile(join(process.cwd(), 'apks/'+filename)))
   }
 }
